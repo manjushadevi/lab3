68a974389f8b9673b3f0c07e635d18af2d812523
manjushadevi <50735541+manjushadevi@users.noreply.github.com>
2021-03-20 09:00:57 +0530

    Add files via upload

--- PositiveString.java
+++ PositiveString.java
@@ -5,31 +5,34 @@ import java.util.Scanner;
 public class PositiveString {
 
 	public static void main(String[] args) {
-		char str1;
-		@SuppressWarnings({ "resource" })
+		@SuppressWarnings("resource")
 		Scanner sc=new Scanner(System.in);
 		String str=sc.next();
 		System.out.println("string :"+str);
-		for(int i=0;i<str.length();i++)
-		{
-			str1=str.charAt(i);
-			System.out.println("string :"+str1+" "+(int)str1);
-			int[] str2=new int[3];
-			for(int i1=0;i1<str1;i1++)
-			{
-				str2[i1]=str1;
-				
-				if(str2[i1]>str2[i1+1])
-				{
-					System.out.println("positive string");
-				}
-				else
-				{
-					System.out.println("negative string");
-				}
-			}
-		}
+		checkPositiveString(str);
 	}
-}
+		public static void checkPositiveString(String s)
+		{
+		int[] a =new int[s.length()];
+		for(int i=0;i<s.length();i++)
+		{
+			a[i]=(int)(s.charAt(i));
+		}
+		for(int i=0;i<s.length()-1;i++)
+		{
+						if(a[i]<a[i+1])
+						{
+							System.out.println("positive string");
+						}
+						else
+						{
+							System.out.println("negative string");
+						}
+					}
+				}
+	}
+
+	
+
 
 
